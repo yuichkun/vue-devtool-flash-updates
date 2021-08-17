2d5b912c87c835fdab783141b3ce44d1621c2a1f
Yuichi Yogo <yuichkun@gmail.com>
2021-08-17 13:13:58 +0900

    reflect initial state of feature availability

--- panel.js
+++ panel.js
@@ -2,11 +2,35 @@ console.log('hello from panel.js')
 const input = document.getElementById('flash-on-update-checkbox')
 const FEATURE_AVAILABILITY_KEY = 'VUE_DEVTOOLS_FLASH_UPDATES_ENABLED'
 
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded')
+  getFeatureAvailability().then(enabled => {
+    console.log('getFeatureAvailability', enabled)
+    input.checked = enabled
+  }).catch(console.error)
+
+})
+
 function onChange(e) {
   console.log('onChange', e)
   setFeatureAvailability(e.target.checked)
 }
 input.addEventListener('change', onChange)
+
+function getFeatureAvailability() {
+  return new Promise((resolve, reject) => {
+    const script = `
+      localStorage.getItem('${FEATURE_AVAILABILITY_KEY}') === 'true'
+    `
+    chrome.devtools.inspectedWindow.eval(script, (result, exception) => {
+      if(exception) {
+        reject(exception)
+        return
+      }
+      resolve(result)
+    })
+  })
+}
 
 function setFeatureAvailability(enabled) {
   const script = `
