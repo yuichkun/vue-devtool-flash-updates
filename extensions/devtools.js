0fe0f900c112ab3972be85253e661787a866f0a6
Yuichi Yogo <yuichkun@gmail.com>
2021-08-17 15:06:39 +0900

    mv directories

---
+++ extensions/devtools.js
@@ -0,0 +1,8 @@
+chrome.devtools.panels.create(
+  "Vue Flash Updates",
+  "vue-flash-updates.png",
+  "panel.html",
+  function (panel) {
+    console.log('successfully created a panel', panel);
+  }
+)
