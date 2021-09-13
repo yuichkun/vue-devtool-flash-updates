5b012d7d836650e282d6753ba486b0cd1d7b0d90
Yuichi Yogo <yuichkun@gmail.com>
2021-09-13 21:50:47 +0900

    use actual package on demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import FlashUpdates from '../../src/vue-devtools-flash-updates'
+import FlashUpdates from 'vue-devtools-flash-updates'
 
 Vue.config.productionTip = false
 
