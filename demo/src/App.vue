f35eb584edc5bcdeb5e3d956af0d6099e7d820d7
Yuichi Yogo <yuichkun@gmail.com>
2021-09-13 17:44:43 +0900

    rename demo component

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
