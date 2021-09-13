fe9bbed7a4f918f0b20a4f4f986e1973cb4ce7d3
Yuichi Yogo <yuichkun@gmail.com>
2021-09-13 15:11:36 +0900

    locally install plugin in the demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FlashUpdates from '../../src/vue-devtools-flash-updates'
 
 Vue.config.productionTip = false
+
+Vue.use(FlashUpdates)
 
 new Vue({
   render: h => h(App),
